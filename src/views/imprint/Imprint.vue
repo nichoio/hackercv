cfe20d0a0734330cf53d9933e9436f47924d20d5
Nicho Kappel <you@example.com>
2021-07-27 21:58:17 +0200

    custom imprint

--- src/views/imprint/Imprint.vue
+++ src/views/imprint/Imprint.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>
-    My Imprint
-  </div>
+  <h2 class="mb-4">
+    Imprint/Impressum
+  </h2>
+  <p>
+    Nicholas Kappel<br>
+    Alte Dorfstraße 12b<br>
+    16230 Melchow, Deutschland
+  </p>
+
+  <p>E-Mail: <a href="#" class="cryptedmail"
+       data-name="work"
+       data-domain="nicho"
+       data-tld="io"
+       onclick="window.location.href = 'mailto:' + this.dataset.name + '@' + this.dataset.domain + '.' + this.dataset.tld; return false;"></a>
+  </p>
 </template>
 
-<script>
-export default {
-}
-</script>
+<style scoped>
+  .cryptedmail:after {
+    content: attr(data-name) "@" attr(data-domain) "." attr(data-tld);
+  }
+</style>
