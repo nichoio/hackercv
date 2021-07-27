2b69b88f943c15ca52c851481eeab5b5b897d672
Nicho Kappel <you@example.com>
2021-07-27 21:52:26 +0200

    use separate CV page

---
+++ src/views/about/CV.vue
@@ -0,0 +1,17 @@
+<template>
+  <View title="CV" >
+    <Timeline class="mb-6" />
+  </View>
+</template>
+
+<script>
+import Timeline from '@/components/Timeline'
+import View from '@/components/layout/View'
+
+export default {
+  components: {
+    Timeline,
+    View
+  }
+}
+</script>
